ab60694cbaa2c5450379645cde51c763adbc61e9
Kevin (Woohyeuk) Lee <ke_vinlee@outlook.com>
2023-11-07 16:49:12 -0600

    navigation more refined, added icons

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homepage from './src/components/Homepage'
-import NotHomepage from './src/components/NotHomepage'
+import Feed from './src/components/Feed'
+import Friends from './src/components/Friends'
+import Me from './src/components/Me'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer style={styles.container}>
-      <Stack.Navigator initialRouteName="NotHome">
-        <Stack.Screen name = "Home" component = {Homepage} />
-        <Stack.Screen name = "NotHome" component = {NotHomepage} />
+      <StatusBar barStyle="light-content" />
+      <Stack.Navigator 
+        initialRouteName="route"
+        screenOptions={{
+        }}>
+        <Stack.Screen 
+          name = "Feed" 
+          component = {Feed} 
+          options={{
+            title: 'Q',
+            headerStyle: {
+              backgroundColor: '#000000'
+            },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen 
+          name = "Friends" 
+          component = {Friends} 
+          options={{
+            headerStyle: {
+              backgroundColor: '#000000'
+            },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen 
+          name = "Me" 
+          component = {Me} 
+          options={{
+            headerStyle: {
+              backgroundColor: '#000000'
+            },
+            headerTintColor: 'white',
+          }}
+        />
       </Stack.Navigator>
-      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
