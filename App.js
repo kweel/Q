6f5711983455cf2f7cc8777e3c5b81e9db42d600
horticulturalhedgemon <ryancheng03@gmail.com>
2023-11-06 19:06:12 -0600

    navigation between two screens

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Homepage from './src/components/Homepage'
+import NotHomepage from './src/components/NotHomepage'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="NotHome">
+        <Stack.Screen name = "Home" component = {Homepage} />
+        <Stack.Screen name = "NotHome" component = {NotHomepage} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
+//a more permanent solution for styles is in the docs:
+//https://reactnavigation.org/docs/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'lightblue',
   },
 });
