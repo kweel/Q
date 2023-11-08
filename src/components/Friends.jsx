54ca176f1e36c95f83630edecff6f8240f3b9403
Kevin (Woohyeuk) Lee <ke_vinlee@outlook.com>
2023-11-07 18:52:26 -0600

    more stuff

---
+++ src/components/Friends.jsx
@@ -0,0 +1,14 @@
+import { Text, View, Button, SafeAreaView, ScrollView } from "react-native";
+import { useLinkProps } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function Friends({ navigation }) {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#000000"}}>
+      <ScrollView>
+        
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
---
+++ src/components/Me.jsx
@@ -0,0 +1,12 @@
+import { Text, View, Button, SafeAreaView } from "react-native";
+import { useLinkProps } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function Me({ navigation }) {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#000000"}}>
+
+    </SafeAreaView>
+  )
+}
