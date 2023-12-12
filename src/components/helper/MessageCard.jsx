779958bddbaa4ed183a6c4ca1f09273d8570cefe
horticulturalhedgemon <ryancheng03@gmail.com>
2023-12-12 01:17:41 -0600

    message to profile button

--- src/components/helper/MessageCard.jsx
+++ src/components/helper/MessageCard.jsx
@@ -2,11 +2,14 @@ import { Text, Button } from "react-native";
 import HelperCard from "./HelperCard"
 import { useContext,useEffect } from "react";
 import GetProfileFunctionContext from "../contexts/GetProfileFunctionContext";
+import { useNavigation } from "@react-navigation/native";
 function MessageCard(props) {
     //fetches data with getProfile and returns it
     const getProfile = useContext(GetProfileFunctionContext)
     const todayQuote = getProfile(props.name,'todayQuote')
-    return <HelperCard style={{ marginTop: 16, padding: 8, marginLeft: 8, marginRight: 8 }}>
+    const navigation = useNavigation()
+    return <HelperCard style={{ marginTop: 16, padding: 8, marginLeft: 8, marginRight: 8 }}
+                onPress={() => navigation.push('Profile',{name:props.name})}>
         <Text style={{fontSize: 28, fontWeight: 600}}>{props.name}</Text>
         <Text style={{fontSize: 20}}>{todayQuote.title}</Text>
         <Text style={{fontSize: 12}}>{todayQuote.body}</Text>
