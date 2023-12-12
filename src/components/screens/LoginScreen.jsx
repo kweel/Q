2044e9841c44c9284fd86664de55ecc40a7b694b
Natalie Cheng <nataliec6041@gmail.com>
2023-12-11 23:17:53 -0600

    some font stuff and editing text input styles

--- src/components/screens/LoginScreen.jsx
+++ src/components/screens/LoginScreen.jsx
@@ -5,24 +5,14 @@ function LoginScreen(props) {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
     return <View style={styles.container}>
-        <Text style={[styles.textHeader, { fontSize: 36 }]}>Q</Text>
-        <Text style={[styles.textHeader, { fontSize: 30, color: '#AAA' }]}>Thinking deeply.</Text>
-        <Text style={[styles.textHeader, { fontSize: 25 }]}>Email</Text>
-        <TextInput style = {{
-            height: 40,
-            margin: 12,
-            borderWidth: 1,
-            padding: 10,
-        }} 
-  onChangeText={setEmail} value = {email}/>
-        <Text style={{ fontSize: 25 }}>Password</Text>
-        <TextInput  style = {{
-            height: 40,
-            margin: 12,
-            borderWidth: 1,
-            padding: 10,
-        }} 
-        secureTextEntry={true} onChangeText={setPassword} value = {password}/>
+        <Text style={[styles.textHeader, { fontSize: 96, marginBottom: 10}]}>Q</Text>
+        <Text style={[styles.textHeader, { fontSize: 20, color: '#AAA', marginBottom: 10 }]}>Thinking deeply.</Text>
+        <Text style={[styles.textHeader, { fontSize: 14 }]}>Email</Text>
+        <TextInput style={styles.textInput}
+  onChangeText={setEmail} value = {email} placeholder="Enter email" keyboardType="email-address" placeholderTextColor="#fff"/>
+        <Text style={[styles.textRegular, { fontSize: 14 }]}>Password</Text>
+        <TextInput style={styles.textInput}
+        secureTextEntry={true} onChangeText={setPassword} value = {password} placeholder= "Enter password" placeholderTextColor="#fff"/>
         <Button color="crimson" title="Login" onPress={() => {
             props.handleLogin(email, password)
         }} />
@@ -34,17 +24,27 @@ function LoginScreen(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#1D1B20',
         alignItems: 'center',
         justifyContent: 'center',
     },
     textHeader: {
-        fontFamily: "Lora-Medium",
+        fontFamily: "Lora-SemiBold",
         color: '#fff'
     },
     textRegular: {
         fontFamily: 'Lora-Regular',
         color: '#fff'
+    },
+    textInput: {
+        backgroundColor: '#1D1B20',
+        borderRadius: 5,
+        borderColor: '#fff',
+        height: 30,
+        width: 300,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     }
 });
 
